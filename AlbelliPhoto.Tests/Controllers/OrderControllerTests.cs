e74015cfeb838ed875ad326bcadc63c133b3ade8
Vahid <vahid.shir@yahoo.com>
2021-12-18 00:10:09 +0330

    write some unit tests

---
+++ AlbelliPhoto.Tests/Controllers/OrderControllerTests.cs
@@ -0,0 +1,198 @@
+﻿using AlbelliPhoto.Abstraction;
+using AlbelliPhoto.Dto;
+using AlbelliPhoto.Server.Controllers;
+
+using AutoFixture;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using NSubstitute;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+
+using Xunit;
+
+namespace AlbelliPhoto.Tests.Controllers
+{
+    public class OrderControllerTests
+    {
+        private readonly Random random;
+        private Fixture fixture;
+
+        public OrderControllerTests()
+        {
+            random = new Random();
+            fixture = new Fixture();
+        }
+
+        [Fact]
+        public void GetOrder_MustReturnNotFound404_GivenNotAnyOrdersFoundInDB()
+        {
+            //Arrange
+            var productOrderServiceMock = Substitute.For<IProductOrderService>();
+            productOrderServiceMock.GetOrder(default).ReturnsForAnyArgs((GetOrderResponse)null);
+
+            var request = new GetOrderRequest { OrderId = fixture.Create<int>() };
+
+            var controller = new OrderController(productOrderServiceMock);
+            int expectedStatusCode = (int)HttpStatusCode.NotFound;
+
+            //Act
+            var result = controller.GetOrder(request) as NotFoundObjectResult;
+
+
+            //Assert
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(expectedStatusCode);
+        }
+
+        [Fact]
+        public void GetOrder_MustReturnAValidOrder_GivenAnyOrdersFoundInDB()
+        {
+            //Arrange
+
+            int orderId = fixture.Create<int>();
+
+            var request = new GetOrderRequest { OrderId = orderId };
+
+            var orderResponse = new GetOrderResponse
+            {
+                OrderId = orderId,
+
+                OrderItems = new List<OrderItemDto>
+                {
+                    new OrderItemDto{ ProductType = ProductType.Calendar, Quantity = 1},
+                    new OrderItemDto{ ProductType = ProductType.Mug, Quantity = 3},
+                    new OrderItemDto{ ProductType = ProductType.Canvas, Quantity = 1},
+                    new OrderItemDto{ ProductType = ProductType.Cards, Quantity = 2}
+                }
+            };
+
+            var productOrderServiceMock = Substitute.For<IProductOrderService>();
+            productOrderServiceMock.GetOrder(request).Returns(orderResponse);
+
+            var controller = new OrderController(productOrderServiceMock);
+            int expectedStatusCode = (int)HttpStatusCode.OK;
+
+            //Act
+            var result = controller.GetOrder(request) as OkObjectResult;
+
+            //Assert
+
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(expectedStatusCode);
+            result.Value.Should().BeOfType<GetOrderResponse>();
+            result.Value.As<GetOrderResponse>().OrderId.Should().Be(orderId);
+        }
+
+        [Fact]
+        public void PlaceOrder_MustReturnALbelliStatusCodeNonZero_GivenRegisteringOrderFailes()
+        {
+            //Arrange
+
+            var placeOrderResponse = new PlaceOrderResponse
+            {
+                AlbelliPhotoStatusCode = random.Next(1, 100)
+            };
+
+            var placeOrderRequest = new PlaceOrderRequest
+            {
+                OrderItems = new List<OrderItemDto> { new OrderItemDto { ProductType = ProductType.Calendar, Quantity = 1 }, }
+            };
+
+            var productOrderServiceMock = Substitute.For<IProductOrderService>();
+            productOrderServiceMock.PlaceOrder(default).ReturnsForAnyArgs(placeOrderResponse);
+
+            var controller = new OrderController(productOrderServiceMock);
+            int expectedStatusCode = (int)HttpStatusCode.InternalServerError;
+
+
+            //Act
+            var result = controller.PlaceOrder(placeOrderRequest) as ObjectResult;
+
+
+            //Assert
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(expectedStatusCode);
+            result.Value.As<PlaceOrderResponse>().AlbelliPhotoStatusCode.Should().Be(placeOrderResponse.AlbelliPhotoStatusCode);
+        }
+
+        //Note: The test below belongs to integration test, but for simplicity I chose to not write integration tests
+        //[Theory]
+        //[ClassData(typeof(PlaceOrderRequestClassData))]
+        private void PlaceOrder_Must_Return_BadRequest400_Given_Customer_Submits_Invalid_Order_Data(PlaceOrderRequest request)
+        {
+            //Arrange
+            var productOrderServiceMock = Substitute.For<IProductOrderService>();
+            //productOrderServiceMock.GetOrder(default).ReturnsForAnyArgs((GetOrderResponse)null);
+
+            var controller = new OrderController(productOrderServiceMock);
+            int expectedStatusCode = (int)HttpStatusCode.BadRequest;
+
+            //Act
+            var result = controller.PlaceOrder(request) as BadRequestObjectResult;
+
+
+            //Assert
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(expectedStatusCode);
+            result.Value.Should().BeNull();
+        }
+
+        private class PlaceOrderRequestClassData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new PlaceOrderRequest
+                    {
+                        OrderItems = new List<OrderItemDto>()
+                        {
+                            new OrderItemDto{ ProductType = null},
+                        }
+                    }
+                };
+
+                yield return new object[]
+                {
+                    new PlaceOrderRequest
+                    {
+                        OrderItems = new List<OrderItemDto>()
+                        {
+                            new OrderItemDto{ ProductType = ProductType.Mug, Quantity = 0},
+                        }
+                    }
+                };
+
+                yield return new object[]
+                {
+                    new PlaceOrderRequest
+                    {
+                        OrderItems = new List<OrderItemDto>()
+                        {
+                            new OrderItemDto{ ProductType = ProductType.Canvas, Quantity = -1},
+                        }
+                    }
+                };
+
+                yield return new object[]
+                {
+                    new PlaceOrderRequest
+                    {
+                        OrderItems = new List<OrderItemDto>()
+                        {
+                            new OrderItemDto{ ProductType = ProductType.Cards, Quantity = 999999999}
+                        }
+                    }
+                };
+            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
---
+++ AlbelliPhoto.Tests/Services/OrderWidthCalculatorTests.cs
@@ -0,0 +1,153 @@
+﻿using AlbelliPhoto.Abstraction;
+using AlbelliPhoto.Abstraction.Entities;
+using AlbelliPhoto.Abstraction.Products;
+using AlbelliPhoto.Dto;
+using AlbelliPhoto.Services;
+
+using AutoFixture;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Logging;
+
+using NSubstitute;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace AlbelliPhoto.Tests.Services
+{
+    public class OrderWidthCalculatorTests
+    {
+        private readonly Random random;
+        private Fixture fixture;
+
+        public OrderWidthCalculatorTests()
+        {
+            random = new Random();
+            fixture = new Fixture();
+        }
+
+        /// <param name="orderItems"></param>
+        /// <param name="expectedTotalWidth"></param>
+        [Theory]
+        [ClassData(typeof(CalculateWidthClassData))]
+        public void Calculate_MustReturnCorrectOrderTotalWidth_GivenDifferentSelectOfSubmittedOrders(IEnumerable<OrderItem> orderItems, float expectedTotalWidth)
+        {
+            //Arrange
+            var productFactoryMock = Substitute.For<IProductFactory>();
+            productFactoryMock.GetProduct(ProductType.PhotoBook).Returns(new PhotoBook());
+            productFactoryMock.GetProduct(ProductType.Calendar).Returns(new Calendar());
+            productFactoryMock.GetProduct(ProductType.Canvas).Returns(new Canvas());
+            productFactoryMock.GetProduct(ProductType.Cards).Returns(new Cards());
+            productFactoryMock.GetProduct(ProductType.Mug).Returns(new Mug());
+
+            var loggerMock = Substitute.For<ILogger<OrderWidthCalculator>>();
+
+            var orderWidthCalculator = new OrderWidthCalculator(productFactoryMock, loggerMock);
+
+            //Act
+            float actualTotalWidth = orderWidthCalculator.Calculate(orderItems);
+
+
+            //Assert
+            actualTotalWidth.Should().BeApproximately(expectedTotalWidth, 0.1F);
+        }
+
+        private class CalculateWidthClassData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.PhotoBook, Quantity = 1},
+                    },
+                    19
+                };
+
+                yield return new object[]
+                 {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.Calendar, Quantity = 1},
+                    },
+                    10
+                 };
+
+                yield return new object[]
+                {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.Canvas, Quantity = 1},
+                    },
+                    16
+                };
+
+                yield return new object[]
+                 {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.Cards, Quantity = 1},
+                    },
+                    4.7
+                 };
+
+                yield return new object[]
+                 {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.PhotoBook, Quantity = 5},
+                        new OrderItem{ ProductType = ProductType.Calendar, Quantity = 4},
+                        new OrderItem{ ProductType = ProductType.Canvas, Quantity = 1},
+                        new OrderItem{ ProductType = ProductType.Cards, Quantity = 3},
+                        new OrderItem{ ProductType = ProductType.Mug, Quantity = 1},
+                    },
+                    259.1
+                 };
+
+                //Here are differant variants for Mug product since it has special business
+                yield return new object[]
+                 {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.Mug, Quantity = 1},
+                    },
+                    94
+                 };
+
+                yield return new object[]
+                 {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.Mug, Quantity = 4},
+                    },
+                    94
+                 };
+
+                yield return new object[]
+                 {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.Mug, Quantity = 5},
+                    },
+                    188
+                 };
+
+                yield return new object[]
+                 {
+                    new List<OrderItem>()
+                    {
+                        new OrderItem{ ProductType = ProductType.Mug, Quantity = 50},
+                    },
+                    1222
+                 };
+            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
